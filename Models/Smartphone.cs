835552e34944156687ab5139c176c186086e4c9c
lLgrando <llggrando@gmail.com>
2022-11-01 09:39:17 -0300

    Primeiro Commit

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,18 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
         public void Ligar()
         {
@@ -22,5 +26,5 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
-    }
+    } 
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("12345678", "NokiaABC", "abcd1234", 50);
+Iphone iphone = new Iphone("87654321", "Iphone10", "4321dcba", 60);
+
+nokia.InstalarAplicativo("Facebook");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+iphone.InstalarAplicativo("Instagram");
+iphone.Ligar();
+iphone.ReceberLigacao();
